c77f81b9655ac6d73090f8326c1d2e9a9f9c4729
khimung <kim2kim@gmail.com>
2017-07-31 02:16:26 -0700

    Update intro, landing, cast, scripts, players, clues

--- vclues-web/src/main/java/com/vclues/controller/DashboardController.java
+++ vclues-web/src/main/java/com/vclues/controller/DashboardController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -55,7 +56,10 @@ public class DashboardController extends BaseController {
     }
     
     @RequestMapping(value = {"/"}, method = RequestMethod.GET)
-    public String index(Model model) {
+    public String index(@RequestHeader("User-Agent") String userAgent, Model model) {
+    	
+    	logger.info(userAgent);
+    	
     	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
     	
     	if (!(auth instanceof AnonymousAuthenticationToken)) {
@@ -65,11 +69,73 @@ public class DashboardController extends BaseController {
     		if(auth.getAuthorities().contains("ROLE_ADMIN")) {
     			return "admin";
     		}
+    		
     		return "internal";
     	}
+
+    	logger.info("going to login");
+    	
+    	/* From registar */
+    	User user = new User();
+        model.addAttribute("userForm", user);
+        //model.addAttribute("content", "signup"); 
     	 
     	model.addAttribute("content", "login");
-        return "index";
+        //return "index";
+    	
+    	if(userAgent.toLowerCase().contains("iphone") && userAgent.toLowerCase().contains("mobile")) {
+    		return "mobile/login";
+    	}
+    	
+    	return "login";
+    }
+
+    /* TEMPORARY */
+    
+    //@RequestMapping(value = {"/"})
+    
+    @RequestMapping(value = {"/home"}, method = RequestMethod.GET)
+    public String home(Model model) {
+    	return "home";
     }
  
+    @RequestMapping(value = {"/cast"}, method = RequestMethod.GET)
+    public String cast(Model model) {
+    	return "cast";
+    }
+
+    @RequestMapping(value = {"/players"}, method = RequestMethod.GET)
+    public String players(Model model) {
+    	return "players";
+    }
+    
+    @RequestMapping(value = {"/videos"}, method = RequestMethod.GET)
+    public String videos(Model model) {
+    	return "videos";
+    }    
+    
+    @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
+    public String test(Model model) {
+    	return "test";
+    }
+    
+    @RequestMapping(value = {"/scripts"}, method = RequestMethod.GET)
+    public String scripts(Model model) {
+    	return "scripts";
+    }    
+    
+    @RequestMapping(value = {"/clues"}, method = RequestMethod.GET)
+    public String clues(Model model) {
+    	return "clues";
+    }
+    
+    @RequestMapping(value = {"/history"}, method = RequestMethod.GET)
+    public String history(Model model) {
+    	return "history";
+    }
+    
+    @RequestMapping(value = {"/suspect"}, method = RequestMethod.GET)
+    public String suspect(Model model) {
+    	return "suspect";
+    }
 }
