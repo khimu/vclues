f6a8c4c109e54bacdf11bd7c65f0935b1b4aace6
khimung <kim2kim@gmail.com>
2017-08-02 00:38:42 -0700

    Responsive complete

--- vclues-web/src/main/java/com/vclues/controller/mobile/DashboardController.java
+++ vclues-web/src/main/java/com/vclues/controller/mobile/DashboardController.java
@@ -1,7 +1,4 @@
-package com.vclues.controller;
-
-import java.util.List;
-import java.util.Map;
+package com.vclues.controller.mobile;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,18 +8,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.vclues.core.app.Constant;
-import com.vclues.core.data.Game;
+import com.vclues.controller.BaseController;
 import com.vclues.core.entity.User;
 import com.vclues.core.service.IGameService;
 import com.vclues.core.service.IStoryService;
 import com.vclues.core.service.IUserService;
 
-@Controller
+@Controller("mobileDashboardController")
+@RequestMapping(value = {"/mobile"})
 public class DashboardController extends BaseController {
 	private final static Logger logger = LoggerFactory.getLogger(DashboardController.class);
 	
@@ -35,31 +31,9 @@ public class DashboardController extends BaseController {
     @Autowired
     private IGameService gameService;
 
-    @RequestMapping(value = {"/welcome"}, method = RequestMethod.GET)
-    public String welcome(Model model) {    
-    	User user = getLoggedInUser();
-		
-    	if(user == null) {
-			logger.info("Not able to retrieve user in session");
-			return "redirect:/login";
-		}		
 
-    	Map<Integer, List<Game>> games = gameService.findAllUserGames(user.getId());
-
-    	model.addAttribute("current", games.get(Constant.CURRENT));
-    	model.addAttribute("past", games.get(Constant.PAST));
-    	
-    	model.addAttribute("stories", storyService.getAllStories());
-    	model.addAttribute("content", "welcome"); 
-    	model.addAttribute("title", "Dashboard");
-    	return "internal";
-    }
-    
-    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
-    public String index(@RequestHeader("User-Agent") String userAgent, Model model) {
-    	
-    	logger.info(userAgent);
-    	
+    @RequestMapping(value = {"/login"}, method = RequestMethod.GET)
+    public String index(Model model) {
     	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
     	
     	if (!(auth instanceof AnonymousAuthenticationToken)) {
@@ -72,8 +46,8 @@ public class DashboardController extends BaseController {
     		
     		return "internal";
     	}
-
-    	logger.info("going to login");
+    	
+    	logger.info("mobile going to login");
     	
     	/* From registar */
     	User user = new User();
@@ -82,14 +56,7 @@ public class DashboardController extends BaseController {
     	 
     	model.addAttribute("content", "login");
         //return "index";
-    	
-    	/*
-    	if(userAgent.toLowerCase().contains("iphone") && userAgent.toLowerCase().contains("mobile")) {
-    		return "mobile/login";
-    	}
-    	*/
-    	
-    	return "login";
+    	return "mobile/login";
     }
 
     /* TEMPORARY */
@@ -98,46 +65,21 @@ public class DashboardController extends BaseController {
     
     @RequestMapping(value = {"/home"}, method = RequestMethod.GET)
     public String home(Model model) {
-    	return "home";
+    	return "mobile/home";
     }
  
     @RequestMapping(value = {"/cast"}, method = RequestMethod.GET)
     public String cast(Model model) {
-    	return "cast";
+    	return "mobile/cast";
     }
 
     @RequestMapping(value = {"/players"}, method = RequestMethod.GET)
     public String players(Model model) {
-    	return "players";
+    	return "mobile/players";
     }
     
     @RequestMapping(value = {"/videos"}, method = RequestMethod.GET)
     public String videos(Model model) {
-    	return "videos";
+    	return "mobile/videos";
     }    
-    
-    @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
-    public String test(Model model) {
-    	return "test";
-    }
-    
-    @RequestMapping(value = {"/scripts"}, method = RequestMethod.GET)
-    public String scripts(Model model) {
-    	return "scripts";
-    }    
-    
-    @RequestMapping(value = {"/clues"}, method = RequestMethod.GET)
-    public String clues(Model model) {
-    	return "clues";
-    }
-    
-    @RequestMapping(value = {"/history"}, method = RequestMethod.GET)
-    public String history(Model model) {
-    	return "history";
-    }
-    
-    @RequestMapping(value = {"/suspect"}, method = RequestMethod.GET)
-    public String suspect(Model model) {
-    	return "suspect";
-    }
 }
